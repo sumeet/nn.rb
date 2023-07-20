9f77978a476c0ed2b0dd578dd1a518e576c0ca4c
Sumeet Agarwal <sumeet.a@gmail.com>
2023-07-19 18:08:03 -0700

    add somec omments

--- twice.rb
+++ twice.rb
@@ -6,6 +6,19 @@ TRAIN = [
     [4, 8],
 ]
 
+# classic linear
+# y = mx+b
+# neuron
+# y = xw+b
+
+EPS = 1e-3
+def dcost_w(w, b)
+    cost_now = cost(w, b)
+    shifted_cost = cost(w+EPS, b)
+    (cost_now - shifted_cost) / EPS
+end
+
+# MSE (Mean Squared Error)
 def cost(w, b)
     cost_fn = 0.0
     TRAIN.each do |x, y_|
@@ -25,7 +38,7 @@ rate = 1e-3
 
 puts "cost: #{cost(w, b)}"
 20_000.times do
-    c = cost(w, b)
+    c = cost(w, b) # want this to be 0
     dw = (cost(w + eps, b) - c) / eps
     db = (cost(w, b + eps) - c) / eps
     w -= rate*dw
